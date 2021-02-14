5478d79390f4dace1e69852bb74bcdad6e19252f
Dizy <mhisf@vip.qq.com>
2021-02-14 00:01:38 +0000

    Fix code style

--- src/treenode.cpp
+++ src/treenode.cpp
@@ -1,120 +1,133 @@
+#include "treenode.h"
 #include <iostream>
 #include <set>
 #include <vector>
-#include "treenode.h"
 
 using namespace std;
 
-
 namespace asttree {
-  set<string> treenode::termAndNontermSet;
-  vector<string> treenode::extraProductionVector;
+set<string> treenode::termAndNontermSet;
+vector<string> treenode::extraProductionVector;
 
-  string treenode::getNewUnTakenNonterm(const string& prefix) {
-    if (treenode::termAndNontermSet.find(prefix) == treenode::termAndNontermSet.end()) {
-      treenode::termAndNontermSet.insert(prefix);
-      return prefix;
-    }
-
-    int i = 1;
-
-    while (true) {
-      string newNonterm = prefix + "_" + to_string(i);
-      if (treenode::termAndNontermSet.find(newNonterm) == treenode::termAndNontermSet.end()) {
-        treenode::termAndNontermSet.insert(newNonterm);
-        return newNonterm;
-      }
-      i++;
-    }
+string treenode::getNewUnTakenNonterm(const string &prefix) {
+  if (treenode::termAndNontermSet.find(prefix) ==
+      treenode::termAndNontermSet.end()) {
+    treenode::termAndNontermSet.insert(prefix);
+    return prefix;
   }
 
-  statement::statement(treenode* t1, treenode* t2, treenode* ete, treenode* t3) : treenode(t1, t2, ete, t3) {
-    parentRewrite = ((terminal*)t1)->value();
-  }
+  int i = 1;
 
-  void quant::doConversion() {
-    beforeConversion();
-    if (numChildren == 1) {
-      base* bs_node = (base*)getChild(0);
-      bs_node->setParent(this);
-      bs_node->doConversion();
-      termOrNonterm = bs_node->termOrNonterm;
-    } else if (numChildren == 2) {
-      base* bs_node = (base*)getChild(0);
-      bs_node->setParent(this);
-      bs_node->doConversion();
-      terminal* t2_node = (terminal*)getChild(1);
-      string bs_name = bs_node->termOrNonterm;
-      if (t2_node->value() == "+") {
-        string productionName = treenode::getNewUnTakenNonterm(parentRewrite + "_quant");
-        string subProductionName = treenode::getNewUnTakenNonterm(productionName + "_sub");
-        string productionRule = bs_name + RULE_OR_SPLITTER + bs_name + " " + subProductionName;
-        string subProductionRule = bs_name + RULE_OR_SPLITTER + bs_name + " " + subProductionName;
-        insertExtraProduction(productionName, productionRule);
-        insertExtraProduction(subProductionName, subProductionRule);
-        termOrNonterm = productionName;
-      } else if (t2_node->value() == "*") {
-        string productionName = treenode::getNewUnTakenNonterm(parentRewrite + "_quant");
-        string productionRule = EPSILON_TRANS + RULE_OR_SPLITTER + bs_name + " " + productionName;
-        insertExtraProduction(productionName, productionRule);
-        termOrNonterm = productionName;
-      }
-    } else if (numChildren == 3) {
-      terminal* t1_node = (terminal*)getChild(0);
-      base* bs_node = (base*)getChild(1);
-      bs_node->setParent(this);
-      bs_node->doConversion();
-      string bs_name = bs_node->termOrNonterm;
-      if (t1_node->value() == "<") {
-        string productionName = treenode::getNewUnTakenNonterm(parentRewrite + "_quant");
-        string productionRule = EPSILON_TRANS + RULE_OR_SPLITTER + bs_name + " " + productionName;
-        insertExtraProduction(productionName, productionRule);
-        termOrNonterm = productionName;
-      } else if (t1_node->value() == "[") {
-        string productionName = treenode::getNewUnTakenNonterm(parentRewrite + "_quant");
-        string productionRule = EPSILON_TRANS + RULE_OR_SPLITTER + bs_name;
-        insertExtraProduction(productionName, productionRule);
-        termOrNonterm = productionName;
-      }
+  while (true) {
+    string newNonterm = prefix + "_" + to_string(i);
+    if (treenode::termAndNontermSet.find(newNonterm) ==
+        treenode::termAndNontermSet.end()) {
+      treenode::termAndNontermSet.insert(newNonterm);
+      return newNonterm;
     }
+    i++;
   }
+}
 
-  void base::doConversion() {
-    beforeConversion();
-    if (numChildren == 1) {
-      term_and_nonterms* nt_node = (term_and_nonterms*)getChild(0);
-      nt_node->doConversion();
-      termOrNonterm = nt_node->termOrNonterm;
-    } else if (numChildren == 3) {
-      subexpr* se_node = (subexpr*)getChild(0);
-      se_node->doConversion();
-      string productionName = treenode::getNewUnTakenNonterm(parentRewrite + "_base");
-      insertExtraProduction(productionName, se_node->termOrNonterm);
+statement::statement(treenode *t1, treenode *t2, treenode *ete, treenode *t3)
+    : treenode(t1, t2, ete, t3) {
+  parentRewrite = ((terminal *)t1)->value();
+}
+
+void quant::doConversion() {
+  beforeConversion();
+  if (numChildren == 1) {
+    base *bs_node = (base *)getChild(0);
+    bs_node->setParent(this);
+    bs_node->doConversion();
+    termOrNonterm = bs_node->termOrNonterm;
+  } else if (numChildren == 2) {
+    base *bs_node = (base *)getChild(0);
+    bs_node->setParent(this);
+    bs_node->doConversion();
+    terminal *t2_node = (terminal *)getChild(1);
+    string bs_name = bs_node->termOrNonterm;
+    if (t2_node->value() == "+") {
+      string productionName =
+          treenode::getNewUnTakenNonterm(parentRewrite + "_quant");
+      string subProductionName =
+          treenode::getNewUnTakenNonterm(productionName + "_sub");
+      string productionRule =
+          bs_name + RULE_OR_SPLITTER + bs_name + " " + subProductionName;
+      string subProductionRule =
+          bs_name + RULE_OR_SPLITTER + bs_name + " " + subProductionName;
+      insertExtraProduction(productionName, productionRule);
+      insertExtraProduction(subProductionName, subProductionRule);
+      termOrNonterm = productionName;
+    } else if (t2_node->value() == "*") {
+      string productionName =
+          treenode::getNewUnTakenNonterm(parentRewrite + "_quant");
+      string productionRule =
+          EPSILON_TRANS + RULE_OR_SPLITTER + bs_name + " " + productionName;
+      insertExtraProduction(productionName, productionRule);
+      termOrNonterm = productionName;
+    }
+  } else if (numChildren == 3) {
+    terminal *t1_node = (terminal *)getChild(0);
+    base *bs_node = (base *)getChild(1);
+    bs_node->setParent(this);
+    bs_node->doConversion();
+    string bs_name = bs_node->termOrNonterm;
+    if (t1_node->value() == "<") {
+      string productionName =
+          treenode::getNewUnTakenNonterm(parentRewrite + "_quant");
+      string productionRule =
+          EPSILON_TRANS + RULE_OR_SPLITTER + bs_name + " " + productionName;
+      insertExtraProduction(productionName, productionRule);
+      termOrNonterm = productionName;
+    } else if (t1_node->value() == "[") {
+      string productionName =
+          treenode::getNewUnTakenNonterm(parentRewrite + "_quant");
+      string productionRule = EPSILON_TRANS + RULE_OR_SPLITTER + bs_name;
+      insertExtraProduction(productionName, productionRule);
       termOrNonterm = productionName;
     }
   }
+}
 
-  term_and_nonterms::term_and_nonterms(treenode* t): treenode(t) {
-    string t_val = ((terminal*)t)->value();
-    treenode::termAndNontermSet.insert(t_val);
-  }
-
-  term_and_nonterms::term_and_nonterms(treenode* t, treenode* tn): treenode(t, tn) {
-    string t_val = ((terminal*)t)->value();
-    treenode::termAndNontermSet.insert(t_val);
-  }
-
-  void term_and_nonterms::doConversion() {
-    beforeConversion();
-    terminal* t1_node = (terminal*)getChild(0);
-    string t1_val = t1_node->value();
-    if (numChildren == 1) {
-      termOrNonterm = t1_val;
-    } else if (numChildren == 2) {
-      term_and_nonterms* nt2_node = (term_and_nonterms*)getChild(1);
-      nt2_node->doConversion();
-      string nt2_val = nt2_node->termOrNonterm;
-      termOrNonterm = t1_val + " " + nt2_val;
-    }
+void base::doConversion() {
+  beforeConversion();
+  if (numChildren == 1) {
+    term_and_nonterms *nt_node = (term_and_nonterms *)getChild(0);
+    nt_node->doConversion();
+    termOrNonterm = nt_node->termOrNonterm;
+  } else if (numChildren == 3) {
+    subexpr *se_node = (subexpr *)getChild(0);
+    se_node->doConversion();
+    string productionName =
+        treenode::getNewUnTakenNonterm(parentRewrite + "_base");
+    insertExtraProduction(productionName, se_node->termOrNonterm);
+    termOrNonterm = productionName;
   }
 }
+
+term_and_nonterms::term_and_nonterms(treenode *t) : treenode(t) {
+  string t_val = ((terminal *)t)->value();
+  treenode::termAndNontermSet.insert(t_val);
+}
+
+term_and_nonterms::term_and_nonterms(treenode *t, treenode *tn)
+    : treenode(t, tn) {
+  string t_val = ((terminal *)t)->value();
+  treenode::termAndNontermSet.insert(t_val);
+}
+
+void term_and_nonterms::doConversion() {
+  beforeConversion();
+  terminal *t1_node = (terminal *)getChild(0);
+  string t1_val = t1_node->value();
+  if (numChildren == 1) {
+    termOrNonterm = t1_val;
+  } else if (numChildren == 2) {
+    term_and_nonterms *nt2_node = (term_and_nonterms *)getChild(1);
+    nt2_node->doConversion();
+    string nt2_val = nt2_node->termOrNonterm;
+    termOrNonterm = t1_val + " " + nt2_val;
+  }
+}
+} // namespace asttree
--- src/treenode.h
+++ src/treenode.h
@@ -2,8 +2,8 @@
 #define TREENODE_H
 
 #include <iostream>
-#include <string>
 #include <set>
+#include <string>
 #include <vector>
 
 using namespace std;
@@ -18,227 +18,214 @@ using namespace std;
 #define EPSILON_TRANS string("/*epsilon*/")
 
 namespace asttree {
-  class treenode;
-  class prog;
-  class statements;
-  class statement;
-  class epsilon_trans_expr;
-  class empty;
-  class expr;
-  class subexpr;
-  class quant;
-  class base;
-  class term_and_nonterms;
-  class terminal;
+class treenode;
+class prog;
+class statements;
+class statement;
+class epsilon_trans_expr;
+class empty;
+class expr;
+class subexpr;
+class quant;
+class base;
+class term_and_nonterms;
+class terminal;
 
-  /*************************************************************
-  class  treenode
+/*************************************************************
+class  treenode
 
-  Represents a node in the abstract syntax tree. The constructor
-  overloads correspond to the number of items on the right-hand-side
-  of rules in your Bison grammar, regardless of whether they
-  are terminals or non-terminals.  We don't construct instances
-  of this class directly---use the subclasses instead.
+Represents a node in the abstract syntax tree. The constructor
+overloads correspond to the number of items on the right-hand-side
+of rules in your Bison grammar, regardless of whether they
+are terminals or non-terminals.  We don't construct instances
+of this class directly---use the subclasses instead.
 
-  *************************************************************/
+*************************************************************/
 
-  class treenode {
-    treenode* children[MAX_CHILD];
-    treenode* parent;
-  protected:
-    int numChildren=0;
-  public:
-    static set<string> termAndNontermSet;
-    static vector<string> extraProductionVector;
-    static string getNewUnTakenNonterm(const string& prefix);
-    string parentRewrite;
-    string termOrNonterm = "";
+class treenode {
+  treenode *children[MAX_CHILD];
+  treenode *parent;
 
-    treenode() : numChildren(0), parent(NULL) {
-      children[0] = NULL;
+protected:
+  int numChildren = 0;
+
+public:
+  static set<string> termAndNontermSet;
+  static vector<string> extraProductionVector;
+  static string getNewUnTakenNonterm(const string &prefix);
+  string parentRewrite;
+  string termOrNonterm = "";
+
+  treenode() : numChildren(0), parent(NULL) { children[0] = NULL; }
+
+  treenode(treenode *t1) : numChildren(1), parent(NULL) { children[0] = t1; }
+
+  treenode(treenode *t1, treenode *t2) : numChildren(2), parent(NULL) {
+    children[0] = t1;
+    children[1] = t2;
+  }
+
+  treenode(treenode *t1, treenode *t2, treenode *t3)
+      : numChildren(3), parent(NULL) {
+    children[0] = t1;
+    children[1] = t2;
+    children[2] = t3;
+  }
+
+  treenode(treenode *t1, treenode *t2, treenode *t3, treenode *t4)
+      : numChildren(4), parent(NULL) {
+    children[0] = t1;
+    children[1] = t2;
+    children[2] = t3;
+    children[3] = t4;
+  }
+
+  void setParent(treenode *p) {
+    if (!(p->parentRewrite).empty()) {
+      parentRewrite = p->parentRewrite;
     }
+    parent = p;
+  }
 
-    treenode(treenode* t1) : numChildren(1), parent(NULL) {
-      children[0] = t1;
-    }
+  treenode *getParent() const { return parent; }
 
-    treenode(treenode* t1, treenode* t2) : numChildren(2), parent(NULL) {
-      children[0] = t1;
-      children[1] = t2;
-    }
-
-
-    treenode(treenode* t1, treenode* t2, treenode* t3) : numChildren(3), parent(NULL) {
-      children[0] = t1;
-      children[1] = t2;
-      children[2] = t3;
-    }
-
-    treenode(treenode* t1, treenode* t2, treenode* t3, treenode* t4) : numChildren(4), parent(NULL) {
-      children[0] = t1;
-      children[1] = t2;
-      children[2] = t3;
-      children[3] = t4;
-    }
-
-    void setParent(treenode* p) {
-      if (!(p->parentRewrite).empty()) {
-        parentRewrite = p->parentRewrite;
-      }
-      parent = p;
-    }
-
-    treenode* getParent() const {
-      return parent;
-    }
-
-    void replace(treenode* t, treenode* with) {
-      for (int i=0; i < numChildren; i++) {
-        if (children[i] != NULL && children[i] == t) {
-          children[i] = with;
-          return;
-        }
+  void replace(treenode *t, treenode *with) {
+    for (int i = 0; i < numChildren; i++) {
+      if (children[i] != NULL && children[i] == t) {
+        children[i] = with;
+        return;
       }
     }
+  }
 
-    virtual void addChild(treenode* t) {
-      children[numChildren++] = t;
-    }
+  virtual void addChild(treenode *t) { children[numChildren++] = t; }
 
-    treenode* getChild(int c) {
-      if (c >= MAX_CHILD || c >= numChildren) {
-        return NULL;
-      }
-
-      return children[c];
-    }
-
-    void setChild(int c, treenode* t) {
-      children[c]=t;
-    }
-
-    virtual string value() const {
+  treenode *getChild(int c) {
+    if (c >= MAX_CHILD || c >= numChildren) {
       return NULL;
     }
 
-    virtual string setValue(string s) {}
+    return children[c];
+  }
 
-    virtual void print(ostream& o) const {
-      for (int i=0; i < numChildren; i++) {
-        if (children[i] != NULL) {
-          children[i]->print(o);
-        }
+  void setChild(int c, treenode *t) { children[c] = t; }
+
+  virtual string value() const { return NULL; }
+
+  virtual string setValue(string s) { return s; }
+
+  virtual void print(ostream &o) const {
+    for (int i = 0; i < numChildren; i++) {
+      if (children[i] != NULL) {
+        children[i]->print(o);
       }
     }
+  }
 
-    virtual void beforeConversion() {
-      for (int i=0; i < numChildren; i++) {
-        if (children[i] != NULL) {
-          children[i]->setParent(this);
-        }
+  virtual void beforeConversion() {
+    for (int i = 0; i < numChildren; i++) {
+      if (children[i] != NULL) {
+        children[i]->setParent(this);
       }
     }
+  }
 
-    virtual void doConversion() {
-      beforeConversion();
-      for (int i=0; i < numChildren; i++) {
-        if (children[i] != NULL) {
-          children[i]->doConversion();
-        }
+  virtual void doConversion() {
+    beforeConversion();
+    for (int i = 0; i < numChildren; i++) {
+      if (children[i] != NULL) {
+        children[i]->doConversion();
       }
     }
+  }
 
-    void insertExtraProduction(const string& name, const string& rule) {
-      extraProductionVector.insert(extraProductionVector.end(), name + NAME_RULE_SPLITTER + rule);
-    }
-  };
+  void insertExtraProduction(const string &name, const string &rule) {
+    extraProductionVector.insert(extraProductionVector.end(),
+                                 name + NAME_RULE_SPLITTER + rule);
+  }
+};
 
-  /* Subclasses should be named after the left-hand-side of each
-    rule in your Bison grammar.  Some limited classes are supplied
-    to you.
-  */
+/* Subclasses should be named after the left-hand-side of each
+  rule in your Bison grammar.  Some limited classes are supplied
+  to you.
+*/
 
-  class prog : public treenode {
-  public:
-    prog(treenode* t1, treenode* ss, treenode* t2) : treenode(t1, ss, t2) {}
-  };
+class prog : public treenode {
+public:
+  prog(treenode *t1, treenode *ss, treenode *t2) : treenode(t1, ss, t2) {}
+};
 
-  class statements: public treenode {
-  public:
-    statements(treenode* s) : treenode(s) {}
-    statements(treenode* s, treenode* ss) : treenode(s, ss) {}
-  };
+class statements : public treenode {
+public:
+  statements(treenode *s) : treenode(s) {}
+  statements(treenode *s, treenode *ss) : treenode(s, ss) {}
+};
 
-  class statement : public treenode {
-  public:
-    statement(treenode* t1, treenode* t2, treenode* ete, treenode* t3);
+class statement : public treenode {
+public:
+  statement(treenode *t1, treenode *t2, treenode *ete, treenode *t3);
 
-    virtual void print(ostream& o) const {
-      treenode::print(o);
-      o << endl << endl;
-    }
-  };
+  virtual void print(ostream &o) const {
+    treenode::print(o);
+    o << endl << endl;
+  }
+};
 
-  class epsilon_trans_expr : public treenode {
-  public:
-    epsilon_trans_expr(treenode* t1, treenode* t2, treenode* e) : treenode(t1, t2, e) {}
-    epsilon_trans_expr(treenode* e) : treenode(e) {}
-  };
+class epsilon_trans_expr : public treenode {
+public:
+  epsilon_trans_expr(treenode *t1, treenode *t2, treenode *e)
+      : treenode(t1, t2, e) {}
+  epsilon_trans_expr(treenode *e) : treenode(e) {}
+};
 
-  class empty : public treenode {
-  };
+class empty : public treenode {};
 
-  class expr : public treenode {
-  public:
-    expr(treenode* se) : treenode(se) {}
-    expr(treenode* se, treenode* t, treenode* e) : treenode(se, t, e) {}
-  };
+class expr : public treenode {
+public:
+  expr(treenode *se) : treenode(se) {}
+  expr(treenode *se, treenode *t, treenode *e) : treenode(se, t, e) {}
+};
 
-  class subexpr : public treenode {
-  public:
-    subexpr(treenode* q) : treenode(q) {}
-    subexpr(treenode* q, treenode* t, treenode* se) : treenode(q, t, se) {}
-  };
+class subexpr : public treenode {
+public:
+  subexpr(treenode *q) : treenode(q) {}
+  subexpr(treenode *q, treenode *t, treenode *se) : treenode(q, t, se) {}
+};
 
-  class quant : public treenode {
-  public:
-    quant(treenode* b) : treenode(b) {}
-    quant(treenode* b, treenode* t) : treenode(b, t) {}
-    quant(treenode* t1, treenode* b, treenode* t2) : treenode(t1, b, t2) {}
-    void doConversion();
-  };
+class quant : public treenode {
+public:
+  quant(treenode *b) : treenode(b) {}
+  quant(treenode *b, treenode *t) : treenode(b, t) {}
+  quant(treenode *t1, treenode *b, treenode *t2) : treenode(t1, b, t2) {}
+  void doConversion();
+};
 
-  class base : public treenode {
-  public:
-    base(treenode* t1, treenode* se, treenode* t2) : treenode(t1, se, t2) {}
-    base(treenode* tn) : treenode(tn) {}
-    void doConversion();
-  };
+class base : public treenode {
+public:
+  base(treenode *t1, treenode *se, treenode *t2) : treenode(t1, se, t2) {}
+  base(treenode *tn) : treenode(tn) {}
+  void doConversion();
+};
 
-  class term_and_nonterms : public treenode {
-  public:
-    term_and_nonterms(treenode* t);
-    term_and_nonterms(treenode* t, treenode* tn);
-    void doConversion();
-  };
+class term_and_nonterms : public treenode {
+public:
+  term_and_nonterms(treenode *t);
+  term_and_nonterms(treenode *t, treenode *tn);
+  void doConversion();
+};
 
-  class terminal : public treenode {
+class terminal : public treenode {
   string s;
-  public:
-    terminal(string v) : s(v) {}
 
-    virtual string setValue(string s1) {
-      s = s1;
-    }
+public:
+  terminal(string v) : s(v) {}
 
-    virtual string value() const {
-      return s;
-    }
+  virtual string setValue(string s1) { s = s1; }
 
-    virtual void print(ostream& o) const {
-      o << s << " ";
-    }
-  };
-}
+  virtual string value() const { return s; }
+
+  virtual void print(ostream &o) const { o << s << " "; }
+};
+} // namespace asttree
 
 #endif
